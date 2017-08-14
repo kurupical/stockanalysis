fdfda9feb8814537fffaeb46bc9f8bef65219268
kurupical <kurupical@gmail.com>
2017-08-14 23:48:49 +0900

    spell_miss

---
+++ python/trade_algorithm.py
@@ -0,0 +1,14 @@
+import trade
+import learn
+
+class TradeAlgorithm:
+# 基底クラス
+    def __init__(self, predictor):
+        self.predictor = predictor
+
+class up_10percent(TradeAlgorism):
+    def __init__(self, predictor):
+        super().__init__(predictor)
+
+    def judge(self):
+        print("making!")
