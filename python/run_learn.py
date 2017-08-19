0de0b708b28686a2313f2f52579ecb54ba46eba6
kurupical <kurupical@gmail.com>
2017-08-19 17:03:07 +0900

    #21

--- python/run_learn.py
+++ python/run_learn.py
@@ -13,7 +13,14 @@ def test():
                                 input_items=["終値"],
                                 output_items=["終値"])
     stock_con.load()
-    stock_con.unit_data()
+    # 学習データの絞り込み１　＠　時価総額 making now
+    # stock_con.search_isinrange_marketcap(min_value=0, max_vaule=10**11)
+    # 学習データの絞り込み２　＠　過去N日
+    stock_con.search_isexist_past_Nday(n_day=200)
+    x, y = stock_con.unit_data()
+    # networkの作成
+
+    # 学習/保存
 
 def main():
     # iniファイルから読むとかいう処理はこっちで後で書く
