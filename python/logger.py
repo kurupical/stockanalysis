fa1758481e0b95ddf2efd5e68e5ad2c4a586336a
kurupical <kurupical@gmail.com>
2017-08-26 15:12:38 +0900

    #44

--- python/logger.py
+++ python/logger.py
@@ -1,16 +1,22 @@
+# stock_analysis library
+
+# common library
+import datetime
 
 class Logger:
 
-    def __init__(self, path, obj):
+    def __init__(self, path, obj=None):
         self.path = path
         self.obj = obj
 
-    def log(self, str, instance):
+    def log(self, log):
         file = open(self.path, 'a')
 
-        # ログに出力する内容の編集
-        date =
-        class_name =
-        str = str
+        now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+        if self.obj is not None:
+            obj_name = self.obj.__class__.__name__
 
-        # ログに出力
+        # ログ出力
+        file = open(self.path, 'a')
+        file.write(str(now) +  "," + str(obj_name) + "," + log + "\n")
+        file.close()
