f5585f34a0109731a74fc297e9742f7bd5543c24
kurupical <kurupical@gmail.com>
2017-08-26 15:13:51 +0900

    #43

--- python/configuration.py
+++ python/configuration.py
@@ -2,12 +2,7 @@
 
 class Configuration:
 
-  @staticmethod
-  def parse_from_file():
-    config = ConfigParser()
-    config.read(file)
-    codes = config['param']['codes']
-    codes = str_to_list(str=codes, split_char=",")
-    start_money = int(config['param']['start_money'])
-    start_date = config['param']['start_date']
-    test_term = int(config['param']['test_term'])
+    @staticmethod
+    def parse_from_file():
+        # iniファイルから変数を取り込む
+        print("making now!")
